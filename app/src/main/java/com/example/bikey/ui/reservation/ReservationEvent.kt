c461d65cac12af37c3e29d0786919021dc350e56
tinaw1412 <jingnan1412@gmail.com>
2025-10-24 09:55:34 -0400

    import uuid for bike

--- app/src/main/java/com/example/bikey/ui/reservation/ReservationEvent.kt
+++ app/src/main/java/com/example/bikey/ui/reservation/ReservationEvent.kt
@@ -1,5 +1,7 @@
 package com.example.bikey.ui.reservation
 
+import java.util.UUID
+
 sealed class ReservationEvent {
     data class Success(val bikeId: UUID) : ReservationEvent()
     data class Failure(val message: String) : ReservationEvent()
