ed92a8d0deed933eace906ad26507331835fe0d0
“Mikachupichu” <michael.pouget01@gmail.com>
2025-10-24 02:46:11 -0400

    Implemented Notifications (Overtime, Reservation Expiry, End of Ride), Enforced Operator Privileges, and Added Real-Time Reservation and Overtime Updates

---
+++ app/src/main/java/com/example/bikey/ui/BMS core and station control/model/Sender.kt
@@ -0,0 +1,5 @@
+package com.example.bikey.ui.BMScoreandstationcontrol.model
+
+interface Sender {
+    fun send(title: String, message: String): Unit
+}
---
+++ app/src/main/java/com/example/bikey/ui/UserContext.kt
@@ -0,0 +1,4 @@
+package com.example.bikey.ui
+
+class UserContext {
+}
