fe3b8f71f0d1a36d4a4847d4fd028d683548fc60
“Mikachupichu” <michael.pouget01@gmail.com>
2025-10-24 09:13:09 -0400

    Forgot to stage my changes

--- app/src/main/java/com/example/bikey/ui/UserContext.kt
+++ app/src/main/java/com/example/bikey/ui/UserContext.kt
@@ -1,4 +1,13 @@
 package com.example.bikey.ui
+import java.util.UUID
+
+class User(val id: UUID = UUID.randomUUID(), val email: String, val isOperator: Boolean = false)
 
 class UserContext {
+    companion object {
+        var user: User? = null
+        val id: UUID? = user?.id
+        val isOperator: Boolean? = user?.isOperator
+        val email: String? = user?.email
+    }
 }
